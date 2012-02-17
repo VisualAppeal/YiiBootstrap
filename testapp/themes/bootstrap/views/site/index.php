0ab3404d5a91157f733d110851cbeea93962154d
Tim Helfensdörfer <tim@visualappeal.de>
2012-02-17 22:52:34 +0100

    Updated frontpage

--- testapp/themes/bootstrap/views/site/index.php
+++ testapp/themes/bootstrap/views/site/index.php
@@ -4,6 +4,9 @@
 	<h1>Welcome to <i><?php echo CHtml::encode(Yii::app()->name); ?></i></h1>
 </div>
 
+<p>The aim of the project is to transform all <a href="http://twitter.github.com/bootstrap/">bootstrap</a> components into <a href="http://www.yiiframework.com/">Yii</a> Widgets and apply the bootstrap style to the default Yii Widgets (like CGridView and CListView). There are also some bootstrap basics like the grid system or the typography which doesn't need widgets.</p>
+
+<h2>Includes</h2>
 <p>This Framework is using some extra software:</p>
 
 <ul>
@@ -11,5 +14,3 @@
 	<li><a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> of course (2.0.0)</li>
 	<li><a href="http://www.yiiframework.com/extension/less/">Yii less extension</a> (0.9)</li>
 </ul>
-
-<p>We will use some <a href="http://dev.w3.org/html5/spec/Overview.html">HTML5</a> Elements and <a href="http://www.w3.org/Style/CSS/specs">CSS3</a>.</p>
