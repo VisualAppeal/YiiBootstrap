8aabe6e89fd49f15caa0d36db09a0f39ec0e7e52
Tim Helfensdörfer <tim@visualappeal.de>
2012-02-19 13:39:00 +0100

    Enhanced frontpage

--- testapp/themes/bootstrap/views/site/index.php
+++ testapp/themes/bootstrap/views/site/index.php
@@ -4,12 +4,27 @@
 $this->renderPartial('//demo/hero');
 ?>
 
-<h2>Includes</h2>
-<p>This Framework uses some 3rd party tools</p>
-
-<ul>
-	<li><a href="http://www.yiiframework.com/">Yii</a> - 1.1.10</li>
-	<li><a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> of course - 2.0.0</li>
-	<li><a href="http://www.yiiframework.com/extension/less/">Yii less extension</a> - 0.9</li>
-	<li><a href="http://placehold.it/">placehold.it</a> - custom thumbnails</li>
-</ul>
+<div class="row">
+	<div class="span3">
+		<h2>How to use</h2>
+		<p>See our custom code examples at the <a href="https://github.com/VisualAppeal/YiiBootstrap/wiki/">github wiki</a>.</p>
+		<p><strong>Easy to integrate into Yii:</strong></p>
+		<ul>
+			<li>Include the <a href="http://twitter.github.com/bootstrap/index.html">bootstrap</a> css/less files</li>
+			<li><a href="http://www.yiiframework.com/extension/yii-bootstrap-widgets/">Download</a> the extension</li>
+			<li>Unpack the extension and move the <em>widgets</em> folder to the <em>protected</em> folder</li>
+			<li>Import the widgets in your config</li>
+		</ul>
+	</div>
+	<div class="span3">
+		<h2>Includes</h2>
+		<p>This Framework uses some 3rd party tools:</p>
+		
+		<ul>
+			<li><a href="http://www.yiiframework.com/">Yii</a> - 1.1.10</li>
+			<li><a href="http://twitter.github.com/bootstrap/">Twitter Bootstrap</a> of course - 2.0.1</li>
+			<li><a href="http://www.yiiframework.com/extension/less/">Yii less extension</a> - 0.9</li>
+			<li><a href="http://placehold.it/">placehold.it</a> - custom thumbnails</li>
+		</ul>
+	</div>
+</div>
