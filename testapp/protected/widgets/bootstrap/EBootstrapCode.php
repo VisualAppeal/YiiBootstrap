71737d8f18b8a6dec7e915fc56192910658eea9f
Tim Helfensdörfer <tim@visualappeal.de>
2012-02-19 16:04:03 +0100

    Added Code widget

--- testapp/protected/widgets/bootstrap/EBootstrapCode.php
+++ testapp/protected/widgets/bootstrap/EBootstrapCode.php
@@ -1,20 +1,62 @@
 <?php 
 
-class EBootstrapCode extends CWidget {
+class EBootstrapCode extends EBootstrapWidget {
+	public $language = '';
+	public $content = '';
+	public $lineNumbers = false;
+	
+	public $htmlOptions = array();
+	
+	public $cssFile = null;
+	public $jsFile = null;
+	
+	public $jsPosition = CClientScript::POS_READY;
+	
 	public function init() {
 		parent::init();
+
+		if (empty($this->content))
+			ob_start();
 		
-		ob_start();
+		if (is_null($this->jsFile)) {
+			$jsFile = dirname(__FILE__).'/js/google-code-prettify/prettify.js';
+			$this->jsFile = Yii::app()->getAssetManager()->publish($jsFile);
+			Yii::app()->clientScript->registerScriptFile($this->jsFile);
+		}
+		if (is_null($this->cssFile)) {
+			$cssFile = dirname(__FILE__).'/js/google-code-prettify/prettify.css';
+			$this->cssFile = Yii::app()->getAssetManager()->publish($cssFile);
+			Yii::app()->clientScript->registerCssFile($this->cssFile);
+		}
+		
+		Yii::app()->clientScript->RegisterScript('ebootstrapcode-prettify.'.$this->getId(), '
+			prettyPrint();
+		', $this->jsPosition);
 	}
 	
 	public function run() {
 		parent::run();
 		
-		$content = ob_get_contents();
-		ob_end_flush();
+		if (empty($this->content)) {
+			$content = ob_get_contents();
+			$content = str_replace('&gt;', '>', $content);
+			$content = str_replace('&lt;', '<', $content);
+			ob_end_clean();
+		}
+		else {
+			$content = $this->content;
+		}
 		
-		echo CHtml::openTag('pre');
+		EBootstrap::mergeClass($this->htmlOptions, array('prettyprint'));
+		if (!empty($this->language))
+			EBootstrap::mergeClass($this->htmlOptions, array('lang-'.$this->language));
+		if ($this->lineNumbers)
+			EBootstrap::mergeClass($this->htmlOptions, array('linenums'));
+		
+		echo CHtml::openTag('pre', $this->htmlOptions);
+
 		echo htmlspecialchars($content);
+		
 		echo CHtml::closeTag('pre');
 	}
 }
