675eac4a8cfc1781d39bbfd7ab6c559ed02a8caa
Tim Helfensdörfer <tim@visualappeal.de>
2012-02-19 03:30:11 +0100

    Allow guest access to crud operations

--- testapp/protected/controllers/CrudController.php
+++ testapp/protected/controllers/CrudController.php
@@ -32,11 +32,11 @@ class CrudController extends Controller
 			),
 			array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				'actions'=>array('create','update'),
-				'users'=>array('@'),
+				'users'=>array('*'),
 			),
 			array('allow', // allow admin user to perform 'admin' and 'delete' actions
 				'actions'=>array('admin','delete'),
-				'users'=>array('admin'),
+				'users'=>array('*'),
 			),
 			array('deny',  // deny all users
 				'users'=>array('*'),
