d6507d4a2f7a53c2862a11c4c479c871deeb8383
Tim Helfensdörfer <tim@visualappeal.de>
2012-02-19 16:04:25 +0100

    Added collapse widget

---
+++ testapp/protected/widgets/bootstrap/EBootstrapCollapse.php
@@ -0,0 +1,111 @@
+<?php 
+
+/*
+ * Creates an collapse sender
+ * This widget creates an element which can be used to toggle a collapse item
+ * http://twitter.github.com/bootstrap/javascript.html#collapse
+ * 
+ * @author Tim Helfensdörfer <tim@visualappeal.de>
+ * @version 0.3.0
+ * @package widgets.bootstrap
+ */
+class EBootstrapCollapse extends EBootstrapWidget {
+	/*
+	 * Html element of the sender
+	 *
+	 * Examples: a, input, span, div, h3...
+	 *
+	 */
+	public $sender = 'a';
+	
+	/*
+	 * Default value
+	 *
+	 * The value is rendered between the sender tags
+	 * Example: <a>$value</a>
+	 */
+	public $value = 'Toggle';
+	
+	/*
+	 * Value if the sender is clicked
+	 * 
+	 * The value is rendered between the sender tags. If it's set to false there will be no change.
+	 * Example: <a>$value</a>
+	 */
+	public $valueToggle = false;
+	
+	/*
+	 * Selector of the target
+	 *
+	 * Examples: '#myElement', '.elements'
+	 */
+	public $target = '';
+	
+	/*
+	 * Selector of the parent element
+	 *
+	 * If it's unequal false, an accordion functionallity will be applied to the child elements
+	 */
+	public $parent = false;
+	
+	/*
+	 * JS file of the collapse plugin
+	 *
+	 * If its set to false, no file will be included
+	 */
+	public $jsFile = null;
+	
+	/*
+	 * Init the widget
+	 */
+	public function init() {
+		parent::init();
+		
+		if (!isset($this->htmlOptions['id']))
+			$this->htmlOptions['id'] = $this->getId();
+
+		if (is_null($this->jsFile)) {
+			$jsFile = dirname(__FILE__).'/js/bootstrap.min.js';
+			$this->jsFile = Yii::app()->getAssetManager()->publish($jsFile);
+			Yii::app()->clientScript->registerScriptFile($this->jsFile);
+		}
+		elseif ($this->jsFile !== false) {
+			Yii::app()->clientScript->registerScriptFile($this->jsFile);
+		}
+		
+		if ($this->valueToggle !== false) {
+			switch ($this->sender) {
+				case 'img':
+				case 'input':
+					$jsFunction = 'val';
+					break;
+				default:
+					$jsFunction = 'html';
+			}
+		
+			Yii::app()->clientScript->registerScript('ebootstrap-collapse-'.$this->getId(), '
+				$("'.$this->target.'").on("show", function() {
+					$("#'.$this->htmlOptions['id'].'").'.$jsFunction.'("'.$this->valueToggle.'");
+				});
+				$("'.$this->target.'").on("hide", function() {
+					$("#'.$this->htmlOptions['id'].'").'.$jsFunction.'("'.$this->value.'");;
+				});
+			', CClientScript::POS_READY);
+		}
+	}
+	
+	/*
+	 * Execute the widget
+	 */
+	public function run() {
+		$this->htmlOptions['data-toggle'] = 'collapse';
+		$this->htmlOptions['data-target'] = $this->target;
+		
+		if ($this->parent !== false)
+			$this->htmlOptions['data-parent'] = $this->parent;
+		
+		echo EBootstrap::tag($this->sender, $this->htmlOptions, $this->value);
+	}
+}
+
+?>
---
+++ testapp/protected/widgets/bootstrap/EBootstrapWidget.php
@@ -0,0 +1,62 @@
+<?php 
+
+/*
+ * Base widget
+ *
+ * This is the parent widget of all bootstrap widgets.
+ * It provides some basic functionallity like the collapse option
+ * 
+ * 
+ * @author Tim Helfensdörfer <tim@visualappeal.de>
+ * @version 0.3.0
+ * @package widgets.bootstrap
+ */
+class EBootstrapWidget extends CWidget {
+	/*
+	 * Collapse the widget
+	 */
+	public $collapse = false;
+	
+	/*
+	 * Show the widget per default
+	 *
+	 * If it's set to true the widget is per default open
+	 */
+	public $collapseShow = false;
+	
+	/*
+	 * Default HTML options
+	 */
+	public $htmlOptions = array();
+	
+	/*
+	 * Javascript file to include for the collapse plugin
+	 *
+	 * If its set to false, no file will be included
+	 */
+	public $collapseJsFile = null;
+	
+	/*
+	 * Init the widget
+	 */
+	public function init() {
+		parent::init();
+		
+		if ($this->collapse) {
+			EBootstrap::mergeClass($this->htmlOptions, array('collapse'));
+			if ($this->collapseShow)
+				EBootstrap::mergeClass($this->htmlOptions, array('in'));
+			
+			if (is_null($this->collapseJsFile)) {
+				$collapseJsFile = dirname(__FILE__).'/js/bootstrap.min.js';
+				$this->collapseJsFile = Yii::app()->getAssetManager()->publish($collapseJsFile);
+				Yii::app()->clientScript->registerScriptFile($this->collapseJsFile);
+			}
+			elseif ($this->collapseJsFile !== false) {
+				Yii::app()->clientScript->registerScriptFile($this->collapseJsFile);
+			}
+		}
+	}
+}
+
+?>
